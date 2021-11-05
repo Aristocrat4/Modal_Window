41126d5c6d3b06b6399cb60eba3ce03c652004db
Aristocrat4 <galadanelia1@gmail.com>
2021-11-05 15:40:53 +0400

    add some click and keydown events

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const popup = document.querySelector('.popup');
+const overlay = document.querySelector('.overlay');
+const openpop = document.querySelectorAll('.btn');
+const closePop = document.querySelector('.close-pop');
+const closingFunc = function (){
+    popup.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const openingFunc = function () {
+    popup.classList.remove('hidden');
+    overlay.classList.remove ('hidden');
+}
+
+for (let i = 0; i < openpop.length; i++) {
+    openpop[i].addEventListener('click', openingFunc)
+}
+closePop.addEventListener('click', closingFunc)
+overlay.addEventListener('click', closingFunc)
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape') {
+        if(!popup.classList.contains('hidden')){
+            closingFunc();
+        }
+    }
+})
